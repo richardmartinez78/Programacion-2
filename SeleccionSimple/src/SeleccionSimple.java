575ee9b1f87e1ee6c0c14e19bd8e66d42ae35318
richardmartinez78 <info.c.b.liceo@gmail.com>
2025-08-27 20:28:42 -0300

    Add files via upload

---
+++ SeleccionSimple/src/SeleccionSimple.java
@@ -0,0 +1,12 @@
+// SeleccionSimple.java
+public class SeleccionSimple
+{
+    public static void main(String args[])
+    {
+        double x = Math.random();
+        if (x < 0.5)
+            System.out.println("Cerca de cero");
+        else
+            System.out.println ("Cerca de uno");
+    }
+}
